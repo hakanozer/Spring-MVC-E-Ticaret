4e7e6d18fcf54cd74a1172530066376a77b14947
ikok2208 <ikok2208@gmail.com>
2016-08-28 12:12:03 +0300

    Admin ve Haberler propertyleri
    Eklendi

---
+++ src/main/java/ozellikler/adminOzellik.java
@@ -0,0 +1,44 @@
+package ozellikler;
+
+public class adminOzellik {
+
+	private String aid;
+	private String aadi;
+	private String asoyadi;
+	private String amail;
+	private String asifre;
+	public String getAid() {
+		return aid;
+	}
+	public void setAid(String aid) {
+		this.aid = aid;
+	}
+	public String getAadi() {
+		return aadi;
+	}
+	public void setAadi(String aadi) {
+		this.aadi = aadi;
+	}
+	public String getAsoyadi() {
+		return asoyadi;
+	}
+	public void setAsoyadi(String asoyadi) {
+		this.asoyadi = asoyadi;
+	}
+	public String getAmail() {
+		return amail;
+	}
+	public void setAmail(String amail) {
+		this.amail = amail;
+	}
+	public String getAsifre() {
+		return asifre;
+	}
+	public void setAsifre(String asifre) {
+		this.asifre = asifre;
+	}
+
+	
+
+	
+}
---
+++ src/main/java/ozellikler/haberEkleOzellik.java
@@ -0,0 +1,45 @@
+package ozellikler;
+
+public class haberEkleOzellik {
+
+	private String haberid;
+	private String haberKat;
+	private String haberAdi;
+	private String haberTarih;
+	private String hicerik;
+	
+	
+	
+	public String getHaberid() {
+		return haberid;
+	}
+	public void setHaberid(String haberid) {
+		this.haberid = haberid;
+	}
+	public String getHaberKat() {
+		return haberKat;
+	}
+	public void setHaberKat(String haberKat) {
+		this.haberKat = haberKat;
+	}
+	public String getHaberAdi() {
+		return haberAdi;
+	}
+	public void setHaberAdi(String haberAdi) {
+		this.haberAdi = haberAdi;
+	}
+	public String getHaberTarih() {
+		return haberTarih;
+	}
+	public void setHaberTarih(String haberTarih) {
+		this.haberTarih = haberTarih;
+	}
+	public String getHicerik() {
+		return hicerik;
+	}
+	public void setHicerik(String hicerik) {
+		this.hicerik = hicerik;
+	}
+	
+	
+}
